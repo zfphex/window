0a77c2e3c773a9e93c1925c54554f7add8c1aae1
Bay <28522740+zX3no@users.noreply.github.com>
2025-07-26 15:06:57 +0930

    cleanup

--- src/main.rs
+++ src/main.rs
@@ -2,12 +2,12 @@ use window::*;
 
 fn main() {
     let mut window = create_window("Window", 0, 0, 600, 400, WindowStyle::DEFAULT);
-    let mut window2 = create_window("Window2", 0, 0, 600, 400, WindowStyle::DEFAULT);
+    // let mut window2 = create_window("Window2", 0, 0, 600, 400, WindowStyle::DEFAULT);
 
     loop {
-        match window2.event() {
-            _ => {}
-        }
+        // match window2.event() {
+        //     _ => {}
+        // }
 
         match window.event() {
             Some(Event::Quit | Event::Input(Key::Escape, _)) => break,
@@ -18,7 +18,8 @@ fn main() {
         window.buffer.fill(0x4fa3a8);
         window.draw();
 
-        window2.buffer.fill(0x165d6a);
-        window2.draw();
+        // window2.buffer.fill(0x165d6a);
+        // window2.draw();
+        return
     }
 }
